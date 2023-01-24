7ebf53f45244702e89d7d21a3ea887da54972138
Antoine C <antoine.cesbron.perso@gmail.com>
2023-01-24 19:33:38 +0100

    [step 1] Add "Bonjour..." and "Au revoir.". I placed "Bonjour..." before text input and not after as requested, it seems more logical. I added some UI text to ask a word and explain that we will check if it's a palindrome.

--- src/main/java/com/ohce/App.java
+++ src/main/java/com/ohce/App.java
@@ -3,16 +3,21 @@ import com.ohce.helpers.PalindromeHelper;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Entrez un mot :");
+        System.out.println("Bonjour," + "\n" + "Entrez un mot et voyons si c'est un palindrome :");
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
         sc.close();
-
         String reverseWord = PalindromeHelper.reverseString(word);
-        System.out.println(reverseWord);
+        System.out.println("Résultat : " + reverseWord);
 
         if(PalindromeHelper.isPalindrome(word)) {
             System.out.println("Bien dit !");
         }
+            else{
+                System.out.println("Raté !");
+            }
+
+
+        System.out.println("Au revoir.");
     }
 }
