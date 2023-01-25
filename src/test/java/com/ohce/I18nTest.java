da37e5b0d6fc5a5de842c6a9b92b5efb969bd3a0
Antoine C <antoine.cesbron.perso@gmail.com>
2023-01-25 10:36:16 +0100

    [step 2] Add i18n manager implementation

--- src/test/java/com/ohce/I18nTest.java
+++ src/test/java/com/ohce/I18nTest.java
@@ -1,17 +1,48 @@
 package com.ohce;
 
+import java.util.Locale;
 import com.ohce.helpers.I18nHelper;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class I18nTest {
+
+    private Locale defaultLocale;
+
+    @Before
+    public void saveDefaultLocale() {
+        defaultLocale = Locale.getDefault();
+    }
+
+    @After
+    public void restorDefaultLocale() {
+        Locale.setDefault(defaultLocale);
+    }
+
     @Test
     public void getMessageDefaultValue(){
         Assert.assertEquals("key.value", I18nHelper.getMessage("key.value"));
     }
+    @Test
+    public void getMessageValueWithEnLocale(){
+        Locale enLocale = new Locale("en_EN");
+
+        Locale.setDefault(enLocale);
+
+        Assert.assertEquals("Hello.", I18nHelper.getMessage("enter.hello"));
+    }
 
     @Test
-    public void getMessageValueWithLocal(){
-        Assert.assertEquals("Bonne matinée.", I18nHelper.getMessage("enter.morning"));
+    public void getMessageValueWithFrLocale(){
+        Locale frLocale = new Locale("fr_FR");
+
+        Locale.setDefault(frLocale);
+
+        Assert.assertEquals("Bonjour.", I18nHelper.getMessage("enter.hello"));
     }
 }
